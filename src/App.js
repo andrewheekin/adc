daaee5f0d254988aebd3f76447fe672b7e610a36
Andrew Heekin <andrewheekin@gmail.com>
2018-03-25 15:08:01 -0400

    add routes to App.js, add the clickit page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import styled from 'styled-components';
+import { mobile, desktop } from './utils/responsive';
 import Links from './components/Links';
-import Content from './components/Content';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import { mobile, desktop } from './utils/responsive';
+import Home from './components/Home';
+import About from './components/About';
+import Project from './components/Project';
+import Contact from './components/Contact';
+import Clickit from './components/Clickit';
+import CS from './components/CS';
 
 const Container = styled.div`
   margin: 0 auto;
   ${desktop} {
-    width: 90%;    
+    width: 90%;
   }
 `;
 
 const Main = styled.div`
   ${desktop} {
-    display: flex;    
+    display: flex;
   }
   ${mobile} {
     display: block;
@@ -29,9 +35,16 @@ class App extends Component {
         <Header />
         <Main>
           <Links />
-          <Content />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/cs" component={CS} />
+            <Route path="/clickit" component={Clickit} />
+            <Route path="/:project" component={Project} />
+          </Switch>
         </Main>
-        <Footer />      
+        <Footer />
       </Container>
     );
   }
---
+++ src/components/Clickit.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Input = styled.input`
+  outline: none;
+  border: none;
+  font-size: 1em;
+  border-radius: 3px;
+  height: 2em;
+  padding: 8px;
+  width: 500px;
+  background: rgba(228, 228, 228, 1);
+  &:hover,
+  &:focus {
+    background: rgba(228, 228, 228, 0.8);
+  }
+`;
+
+const Button = styled.button`
+  margin: 0 0 0 10px;
+  border-radius: 3px;
+  cursor: pointer;
+  transition: 0.1s ease all;
+  &:hover,
+  &:focus {
+    background: rgba(228, 228, 228, 0.3);
+  }
+`;
+
+const Label = styled.div`
+  font-size: 0.8em;
+  opacity: 0.6;
+
+`;
+
+const Container = styled.div`
+  display: 'flex';
+`;
+
+class Clickit extends React.Component {
+  state = { url: 'https://google.com/' };
+
+  handleChange = e => this.setState({ url: e.target.value });
+
+  handleStart = async () => {
+    if (!this.state.url) return;
+    this.scrape();
+
+  };
+
+  handleStop = () => {
+
+  };
+
+  scrape = async () => {
+    let response = await fetch(this.state.url);
+    let page = await response.text();
+    let anchorTags = page.match(/<a.*<\/a>/g);
+    let hrefs = anchorTags.map(a => a.match(/http.*"/g)[0]);
+  }
+
+  render() {
+    return (
+      <Container>
+        <Label>URL</Label>
+        <div style={{ display: 'flex' }}>
+          <Input type="text" onChange={this.handleChange} value={this.state.url} autoFocus />
+          <Button onClick={this.handleStart}>Click new links over next 5 min</Button>
+          <Button onClick={this.handleStop}>Stop</Button>
+        </div>
+      </Container>
+    );
+  }
+}
+
+export default Clickit;
